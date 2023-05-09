973058061d10aff14df7b8f4ecc765a43d49fde8
VTDo <115161122+VT-Do@users.noreply.github.com>
2023-05-09 21:12:17 +0200

    Update extract_ids.py

--- extract_ids.py
+++ extract_ids.py
@@ -52,15 +52,7 @@ def download(output):
     		file_name='data.csv',
     		mime='text/csv',
 		)
-	
-        # Define the CSS styles for the header row
-        header_style = '''
-            text-align: center;
-            font-weight: bold;
-            '''
-        # Apply the CSS styles to the header row of the dataframe
-        styled_df = df.style.set_properties(**{'text-align': 'center'}).set_table_styles([{'selector': 'th', 'props': [('text-align', 'center'), ('font-weight', 'bold')]}])
-        st.write(styled_output)
+        st.write(output)
     else:
         st.write('')
         st.write('No output found')
