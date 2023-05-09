0905123a74efb31849dc5385a4fe621d3672c009
VTDo <115161122+VT-Do@users.noreply.github.com>
2023-05-09 18:02:37 +0200

    Update extract_ids.py

--- extract_ids.py
+++ extract_ids.py
@@ -42,7 +42,7 @@ with col3:
 st.markdown("""<hr style="height:1px;border:none;color:#333;background-color:#95e8a4;" /> """, unsafe_allow_html=True)
 choice = st.sidebar.radio("Select the store",('PlayStore','AppStore'), horizontal=True)
     
-list_bundleid = st.sidebar.text_area('Put lines here', 'Ex: [1,2]')
+
 
 try:
     list_bundleid=ast.literal_eval(list_bundleid)
@@ -53,31 +53,32 @@ app_data = []
 
         
 
-if (choice=="PlayStore") and (list_bundleid!='Ex: [1,2]'):
+if (choice=="PlayStore"):
     st.sidebar.write('Hello PlayStore')	
-    
-    for bundle_id in list_bundleid:
-        try:
-            url = "https://play.google.com/store/apps/details?id=" + bundle_id
-            response = requests.get(url)
-            soup = BeautifulSoup(response.text, "html.parser")
-            title_element = soup.find("h1",class_=re.compile("Fd93Bb"))
-            app_title = title_element.text.strip()
-            app_data.append({'Bundle ID': bundle_id, 'App Title': app_title})
-        except:
+    list_bundleid = st.sidebar.text_area('Put lines here', '['air.com.jogatina.ginrummy.android','air.com.jogatina.mahjong']')
+    if (list_bundleid!='Ex: [1,2]'):
+        for bundle_id in list_bundleid:
             try:
-                url = "https://apptopia.com/google-play/app/" + bundle_id + "/about"
+                url = "https://play.google.com/store/apps/details?id=" + bundle_id
                 response = requests.get(url)
                 soup = BeautifulSoup(response.text, "html.parser")
-                json_element = soup.find("script", type="application/ld+json")
-                json_data = json.loads(json_element.string)
-                app_title = json_data['name']
+                title_element = soup.find("h1",class_=re.compile("Fd93Bb"))
+                app_title = title_element.text.strip()
                 app_data.append({'Bundle ID': bundle_id, 'App Title': app_title})
             except:
-                app_data.append({'Bundle ID': bundle_id, 'App Title': '-'})	
+                try:
+                    url = "https://apptopia.com/google-play/app/" + bundle_id + "/about"
+                    response = requests.get(url)
+                    soup = BeautifulSoup(response.text, "html.parser")
+                    json_element = soup.find("script", type="application/ld+json")
+                    json_data = json.loads(json_element.string)
+                    app_title = json_data['name']
+                    app_data.append({'Bundle ID': bundle_id, 'App Title': app_title})
+                except:
+                    app_data.append({'Bundle ID': bundle_id, 'App Title': '-'})	
 elif (choice=="AppStore"):
     st.sidebar.write('Hello AppStore')
-				 
+    list_bundleid = st.sidebar.text_area('Put bundleid list here', '[1,2]')			 
        
 	
 
