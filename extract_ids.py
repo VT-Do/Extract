4b1778e37bb5104be8d3b731635c8ed74cff377f
VTDo <115161122+VT-Do@users.noreply.github.com>
2023-05-09 16:35:48 +0200

    Update extract_ids.py

--- extract_ids.py
+++ extract_ids.py
@@ -29,13 +29,7 @@ def download(output):
       
 choice = st.sidebar.radio("Select the store",('PlayStore','AppStore'), horizontal=True)
 
-col4, col5, col6 = st.columns((4, 7, 1))
-    with col4:
-        st.write('')
-    with col5:
-        st.title("Extracted Bundle IDs") 
-    with col6:
-        st.write('')
+
 
 st.markdown("""<hr style="height:1px;border:none;color:#333;background-color:#95e8a4;" /> """, unsafe_allow_html=True)
     
