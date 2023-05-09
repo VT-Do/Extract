277cf4e7c946da11ec4d9ebe2eaa6f3c56c67adf
VTDo <115161122+VT-Do@users.noreply.github.com>
2023-05-09 18:14:00 +0200

    Update extract_ids.py

--- extract_ids.py
+++ extract_ids.py
@@ -56,7 +56,7 @@ app_data = []
 if (choice=="PlayStore"):
     st.sidebar.write('Hello PlayStore')	
     list_bundleid = st.sidebar.text_area('Put lines here', "['air.com.jogatina.ginrummy.android','air.com.jogatina.mahjong']")
-    if (list_bundleid!='Ex: [1,2]'):
+    if (list_bundleid!="['air.com.jogatina.ginrummy.android','air.com.jogatina.mahjong']"):
         for bundle_id in list_bundleid:
             try:
                 url = "https://play.google.com/store/apps/details?id=" + bundle_id
@@ -78,7 +78,27 @@ if (choice=="PlayStore"):
                     app_data.append({'Bundle ID': bundle_id, 'App Title': '-'})	
 elif (choice=="AppStore"):
     st.sidebar.write('Hello AppStore')
-    list_bundleid = st.sidebar.text_area('Put bundleid list here', '[1,2]')			 
+    list_bundleid = st.sidebar.text_area('Put bundleid list here', '[1,2]')
+    if (list_bundleid!='[1,2]'):
+        for bundle_id in list_bundleid:
+            try:
+                url = "https://play.google.com/store/apps/details?id=" + bundle_id
+                response = requests.get(url)
+                soup = BeautifulSoup(response.text, "html.parser")
+                title_element = soup.find("h1",class_=re.compile("Fd93Bb"))
+                app_title = title_element.text.strip()
+                app_data.append({'Bundle ID': bundle_id, 'App Title': app_title})
+            except:
+                try:
+                    url = "https://apptopia.com/google-play/app/" + bundle_id + "/about"
+                    response = requests.get(url)
+                    soup = BeautifulSoup(response.text, "html.parser")
+                    json_element = soup.find("script", type="application/ld+json")
+                    json_data = json.loads(json_element.string)
+                    app_title = json_data['name']
+                    app_data.append({'Bundle ID': bundle_id, 'App Title': app_title})
+                except:
+                    app_data.append({'Bundle ID': bundle_id, 'App Title': '-'})	
        
 	
 
