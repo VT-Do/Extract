79c21945d2e653dbfaff8d4c99094acf7697a36f
VTDo <115161122+VT-Do@users.noreply.github.com>
2023-05-09 21:50:50 +0200

    Update extract_ids.py

--- extract_ids.py
+++ extract_ids.py
@@ -12,7 +12,7 @@ import ast
 st.set_page_config(layout="wide")
 #st.sidebar.write('Hello')
 def get_data(input):
-    if (input!="Example: ['air.com.jogatina.ginrummy.android','air.com.jogatina.mahjong']"):
+    if (input!="Example: ['air.com.jogatina.ginrummy.android','air.com.jogatina.mahjong']") and (input!="Example: [1,2]"):
         try:
             list_bundleid=ast.literal_eval(input)
         except:
