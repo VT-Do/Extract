34ceca459496c966000f8b3fef74c0c1b2a3b966
VTDo <115161122+VT-Do@users.noreply.github.com>
2023-05-10 21:35:58 +0200

    Update extract_ids.py

--- extract_ids.py
+++ extract_ids.py
@@ -89,16 +89,13 @@ def download(output):
         col1, col2, col3= st.columns((7,2,2))
         with col1:	
             pd.set_option('display.max_colwidth', None)
-            st.markdown(
-    f"""
-    <style>
-    th {{
+            st.write(f"""
+<style>
+    table th {{
         text-align: center;
     }}
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+</style>
+""", unsafe_allow_html=True)
             st.dataframe(output.reset_index(drop=True))
         with col2:
             container=st.container()
