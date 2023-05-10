1f73ef34792059cd13ef95ec248c2dfc3630cf23
VTDo <115161122+VT-Do@users.noreply.github.com>
2023-05-10 08:29:21 +0200

    Update extract_ids.py

--- extract_ids.py
+++ extract_ids.py
@@ -43,7 +43,7 @@ def playstore_data(input):
     return app_data
 	
 def appstore_data(input):
-    if (input!="Example: [1331794412]"):
+    if (input!="Example: ['1331794412']"):
         try:
             list_bundleid=ast.literal_eval(input)
         except:
