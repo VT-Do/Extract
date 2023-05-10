f0178e8b9fdeba52d582c2a7fb61c4e164d34d1d
VTDo <115161122+VT-Do@users.noreply.github.com>
2023-05-10 22:18:24 +0200

    Update extract_ids.py

--- extract_ids.py
+++ extract_ids.py
@@ -21,10 +21,10 @@ def playstore_data(input):
             if '\n' in list:
                 list=re.sub("[,]", "", list)
                 list_bundleid=list.split(sep='\n')
-                list_bundleid = [x for x in list_bundleid if  not x.isspace() ]
+                list_bundleid = [x for x in list_bundleid if  not x.isspace() and x != '' ]
             else:
                 list_bundleid=list.split(sep=',')
-                list_bundleid = [x for x in list_bundleid if not x.isspace()]
+                list_bundleid = [x for x in list_bundleid if not x.isspace() and x != '']
         except:
             st.warning('Please check the input')
         for bundle_id in list_bundleid:
@@ -58,10 +58,10 @@ def appstore_data(input):
             if '\n' in list:
                 list=re.sub("[,]", "", list)
                 list_bundleid=list.split(sep='\n')
-                list_bundleid = [x for x in list_bundleid if  not x.isspace() ]
+                list_bundleid = [x for x in list_bundleid if not x.isspace() and x != '' ]
             else:
                 list_bundleid=list.split(sep=',')
-                list_bundleid = [x for x in list_bundleid if  not x.isspace() ]
+                list_bundleid = [x for x in list_bundleid if not x.isspace() and x != '']
         except:
                 st.warning('Please check the input')
         for bundle_id in list_bundleid:
